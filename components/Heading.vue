85ac6e71f37bf882ca8b38fd584830304e9cbe33
Lina <75808764+nullium21@users.noreply.github.com>
2023-05-03 19:39:25 +0300

    Move PageSection header into own component

---
+++ components/Heading.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 :class="cls">
+    <slot />
+  </h1>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  small?: boolean
+}>();
+
+const cls = `${props.small ? 'text-xl' : 'text-2xl'} grow`;
+</script>
--- components/PageSection.vue
+++ components/PageSection.vue
@@ -2,7 +2,7 @@
   <component :is="el" class="flex flex-row p-6 space-x-6 items-center font-display" :href="props.href">
     <slot>
       <img :class="iconClass" :src="props.icon" v-if="props.icon">
-      <h1 :class="nameClass">{{ props.left }}</h1>
+      <Heading :small="props.small">{{ props.left }}</Heading>
       <h3 :class="valueClass">{{ props.right }}</h3>
     </slot>
   </component>
@@ -20,6 +20,5 @@ const props = defineProps<{
 const el = (props.href != null) ? 'a' : 'section';
 
 const iconClass = `rounded-full ${props.small ? 'w-12' : 'w-16'} ${props.small ? 'h-12' : 'h-16'}`;
-const nameClass = `${props.small ? 'text-xl' : 'text-2xl'} grow`;
 const valueClass = `${props.small ? 'text-lg' : 'text-xl'} ${el === 'a' ? 'text-sky-700' : 'text-slate-700'}`;
 </script>
