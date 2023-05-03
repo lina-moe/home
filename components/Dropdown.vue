845cb8765477ccee8f6553fa198553337d734587
Lina <75808764+nullium21@users.noreply.github.com>
2023-05-03 19:40:54 +0300

    Add Dropdown/accordion component

---
+++ components/Dropdown.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <input class="hidden" type="checkbox" :id="inputId">
+  <div class="dropdown-container flex flex-col p-6 space-y-6 font-display">
+    <label class="flex flex-row" :for="inputId">
+      <Heading>{{ props.name }}</Heading>
+    </label>
+    <ul class="dropdown-content">
+      <slot />
+    </ul>
+  </div>
+
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  name: string,
+}>();
+
+const inputId = `dropdown-${Math.random().toString(36).substring(2)}`;
+</script>
+<style lang="postcss">
+input ~ .dropdown-container>.dropdown-content {
+  display: none;
+}
+
+input:checked ~ .dropdown-container>.dropdown-content {
+  display: block;
+}
+input:checked ~ .dropdown-container {
+  @apply bg-slate-200;
+}
+</style>
