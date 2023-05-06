5a3cc78f127b8c402eef27031277c31fc17d70ca
Lina <75808764+nullium21@users.noreply.github.com>
2023-05-06 12:20:15 +0300

    Do not actually use trpc-nuxt
    The package seems to not build -_- because of some "#imports" BS.

--- plugins/client.ts
+++ plugins/client.ts
@@ -1,17 +1,68 @@
-import { createTRPCNuxtClient, httpBatchLink } from 'trpc-nuxt/client';
+import { createTRPCProxyClient } from '@trpc/client';
+import { createFlatProxy, createRecursiveProxy } from '@trpc/server/shared';
 import type { AppRouter } from '~/server/api/trpc/[trpc]';
 
+import { hash } from 'ohash';
+import { httpLink as _httpLink, httpBatchLink as _httpBatchLink } from '@trpc/client';
+import { type FetchEsque } from '@trpc/client/dist/internals/types';
+import { FetchError } from 'ofetch';
+
+// Code adapted from trpc-nuxt: client/index.ts
+
 export default defineNuxtPlugin(() => {
-  const client = createTRPCNuxtClient<AppRouter>({
+  // const client = createTRPCNuxtClient<AppRouter>({
+  //   links: [
+  //     httpBatchLink({
+  //       url: '/api/trpc',
+  //     }),
+  //   ],
+  // })
+
+  const headers = useRequestHeaders();
+  const client = createTRPCProxyClient<AppRouter>({
     links: [
-      httpBatchLink({
+      _httpBatchLink<AppRouter>({
         url: '/api/trpc',
-      }),
-    ],
-  })
+        headers() { return headers },
+        fetch: ((input, init?: RequestInit & { method: 'GET' }) =>
+          $fetch.raw(input.toString(), init)
+          .catch(e => {
+            if (e instanceof FetchError && e.response) { return e.response; }
+            throw e;
+          })
+          .then(response => ({ ...response, json: () => Promise.resolve(response._data) }))) as FetchEsque
+      })
+    ]
+  });
+
   return {
     provide: {
-      client,
+      client: createFlatProxy(name => createRecursiveProxy(opts => {
+        const args = opts.args;
+        const [input, other] = args;
+        
+        const segments = [name, ...opts.path];
+        const last = segments.pop()!;
+        const path = segments.join('.');
+
+        if (last === 'useQuery') {
+          const { trpc, ...asyncDataOptions } = other || {} as any;
+
+          let controller: AbortController;
+          if (trpc?.abortOnUnmount) {
+            if (getCurrentInstance()) onScopeDispose(() => controller?.abort());
+            controller = typeof AbortController !== 'undefined' ? new AbortController() : {} as AbortController;
+          }
+
+          const key = input === undefined ? path : `${path}-${hash(input || '')}`;
+          return useAsyncData(key, () => (client as any)[path].query(input, {
+            signal: controller?.signal,
+            ...trpc
+          }), asyncDataOptions);
+        }
+
+        return (client as any)[path][last](...args);
+      }))
     },
   }
 });
--- server/api/trpc/[trpc].ts
+++ server/api/trpc/[trpc].ts
@@ -1,5 +1,12 @@
 import { procedure, router } from "~/server/trpc";
-import { createNuxtApiHandler } from 'trpc-nuxt';
+// import { createNuxtApiHandler } from 'trpc-nuxt';
+
+import { createURL } from 'ufo';
+import { TRPCError } from "@trpc/server";
+import { resolveHTTPResponse } from "@trpc/server/http";
+import { createContext } from "vm";
+
+// Code adapted from trpc-nuxt: index.ts
 
 export type AppRouter = typeof appRouter;
 export const appRouter = router({
@@ -12,6 +19,44 @@ export const appRouter = router({
     ]))
 });
 
-export default createNuxtApiHandler({
-  router: appRouter
+export default defineEventHandler(async (event) => {
+  const { req, res } = event.node;
+  const $url = createURL(req.url!);
+
+  const path = (() => {
+    const { params } = event.context;
+    if (typeof params?.trpc === 'string') return params.trpc;
+
+    if (params?.trpc && Array.isArray(params.trpc))
+      return (params.trpc as string[]).join('/');
+    
+    return null;
+  })();
+
+  if (path === null) throw createError({
+    statusCode: 500,
+    statusMessage: JSON.stringify(appRouter.getErrorShape({
+      error: new TRPCError({ message: 'Param "trpc" not found - is the file named `[trpc]`.ts or `[...trpc].ts`?', code: 'INTERNAL_SERVER_ERROR' }),
+      type: 'unknown', ctx: undefined, path: undefined, input: undefined
+    }))
+  });
+
+  const { status, headers, body } = await resolveHTTPResponse({
+    router: appRouter,
+    req: {
+      method: req.method!, headers: req.headers,
+      body: isMethod(event, 'GET') ? null : await readBody(event),
+      query: $url.searchParams
+    },
+    path,
+    createContext: async () => createContext?.(event)
+  });
+
+  res.statusCode = status;
+  if (headers) Object.keys(headers).forEach(key => res.setHeader(key, headers[key]!));
+  return body;
 });
+
+// export default createNuxtApiHandler({
+//   router: appRouter
+// });
